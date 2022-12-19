122a10d4b5ad0c9a1189c0e6205bf5395871c907
satisfly <vilkenbraadress@gmail.com>
2022-12-19 14:59:30 +0100

    Done but with ugly css, searchbar for catecory and name no extras yet

---
+++ Storage/Storage/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [StringLength(64, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+
+        public int Price { get; set; }
+
+        [Display(Name = "Beställningsdatum"), DataType(DataType.Date)]
+        public DateTime Orderdate { get; set; }
+
+        [Required, StringLength(64, MinimumLength = 2)]
+        public string Category { get; set; } = string.Empty;
+
+        public string Shelf { get; set; } = string.Empty;
+
+        public int Count { get; set; }
+
+        public string Description { get; set; }
+
+    }
+
+}
---
+++ Storage/Storage/Models/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Storage.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; } = 0;
+        public int Count { get; set; } = 0;
+        public int InventoryValue { get; set; } = 0;
+    }
+    
+}
