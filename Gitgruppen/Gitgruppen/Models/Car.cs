b52620bf7cd671e0a3c785bd81690d5c2577226b
satisfly <vilkenbraadress@gmail.com>
2022-12-21 16:36:40 +0100

    Add Car model

---
+++ Gitgruppen/Gitgruppen/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace Gitgruppen.Models
+{
+    public class Car
+    {
+        public int id { get; set; }
+        public string brand { get; set; }
+        public string color { get; set; }
+    }
+}
