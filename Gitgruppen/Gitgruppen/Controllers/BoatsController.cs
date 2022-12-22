f16ac93b6b1d8170725a0c688f3425bba13f67ae
satisfly <vilkenbraadress@gmail.com>
2022-12-22 08:41:59 +0100

    Generated BoatController

---
+++ Gitgruppen/Gitgruppen/Controllers/BoatsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gitgruppen.Data;
+using Gitgruppen.Models;
+
+namespace Gitgruppen.Controllers
+{
+    public class BoatsController : Controller
+    {
+        private readonly GitgruppenContext _context;
+
+        public BoatsController(GitgruppenContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Boats
+        public async Task<IActionResult> Index()
+        {
+              return _context.Boat != null ? 
+                          View(await _context.Boat.ToListAsync()) :
+                          Problem("Entity set 'GitgruppenContext.Boat'  is null.");
+        }
+
+        // GET: Boats/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Boat == null)
+            {
+                return NotFound();
+            }
+
+            var boat = await _context.Boat
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (boat == null)
+            {
+                return NotFound();
+            }
+
+            return View(boat);
+        }
+
+        // GET: Boats/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Boats/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,brand,color")] Boat boat)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(boat);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(boat);
+        }
+
+        // GET: Boats/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Boat == null)
+            {
+                return NotFound();
+            }
+
+            var boat = await _context.Boat.FindAsync(id);
+            if (boat == null)
+            {
+                return NotFound();
+            }
+            return View(boat);
+        }
+
+        // POST: Boats/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,brand,color")] Boat boat)
+        {
+            if (id != boat.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(boat);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BoatExists(boat.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(boat);
+        }
+
+        // GET: Boats/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Boat == null)
+            {
+                return NotFound();
+            }
+
+            var boat = await _context.Boat
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (boat == null)
+            {
+                return NotFound();
+            }
+
+            return View(boat);
+        }
+
+        // POST: Boats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Boat == null)
+            {
+                return Problem("Entity set 'GitgruppenContext.Boat'  is null.");
+            }
+            var boat = await _context.Boat.FindAsync(id);
+            if (boat != null)
+            {
+                _context.Boat.Remove(boat);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BoatExists(int id)
+        {
+          return (_context.Boat?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Gitgruppen/Gitgruppen/Data/GitgruppenContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gitgruppen.Models;
+
+namespace Gitgruppen.Data
+{
+    public class GitgruppenContext : DbContext
+    {
+        public GitgruppenContext (DbContextOptions<GitgruppenContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gitgruppen.Models.Boat> Boat { get; set; } = default!;
+    }
+}
