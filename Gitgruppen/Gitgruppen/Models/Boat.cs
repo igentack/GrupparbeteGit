de04ce581f4c641bb6576dcb21e2c1e2d2905986
satisfly <vilkenbraadress@gmail.com>
2022-12-22 07:13:24 +0100

    New boat model

---
+++ Gitgruppen/Gitgruppen/Models/Boat.cs
@@ -0,0 +1,10 @@
+﻿namespace Gitgruppen.Models
+{
+    public class Boat
+    {
+        public int id { get; set; }
+        public string brand { get; set; }
+        public string color { get; set; }
+
+    }
+}
