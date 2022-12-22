849093d11637cbf9e20c87576b8739e122a5bea6
satisfly <vilkenbraadress@gmail.com>
2022-12-22 10:21:41 +0100

    Id car

--- Gitgruppen/Gitgruppen/Models/Car.cs
+++ Gitgruppen/Gitgruppen/Models/Car.cs
@@ -2,7 +2,7 @@
 {
     public class Car
     {
-        public int Id { get; set; }
+        public int id { get; set; }
         public string Brand { get; set; }
         public string Color { get; set; }
         public string Company { get; set; }
