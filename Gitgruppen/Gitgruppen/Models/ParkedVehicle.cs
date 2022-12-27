c1cb4d1892f48a78a2b9bb577814a5eb6b482f59
satisfly <vilkenbraadress@gmail.com>
2022-12-28 00:15:21 +0100

    Some TODOs and altered Arrived prop to private set

--- Gitgruppen/Gitgruppen/Models/ParkedVehicle.cs
+++ Gitgruppen/Gitgruppen/Models/ParkedVehicle.cs
@@ -10,7 +10,12 @@ namespace Gitgruppen.Models
         public string LicensePlate { get; set; }
         public Type Type { get; set; }
 
-        public DateTime Arrived { get; set; } = DateTime.Now;
+        //[DatabaseGenerated(DatabaseGeneratedOption.Computed)] <= fick ej denna att fungera
+        public DateTime Arrived { get; private set; } = DateTime.Now;
+
+        // TODO Kanske implementera en [Not Mapped] prop som räknar ut parkeringstiden?
+
+        // TODO Enligt bonusuppgiften "Parkeradtid". 
 
         public string Color { get; set; }
         public string Brand { get; set; }
