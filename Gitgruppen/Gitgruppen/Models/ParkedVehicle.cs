f165205dc8b1548e279e881bc42b4eb1a714fd79
Dennis Hagenäs <dh_lexicon@outlook.com>
2022-12-27 15:19:32 +0100

    Added datetime

--- Gitgruppen/Gitgruppen/Models/ParkedVehicle.cs
+++ Gitgruppen/Gitgruppen/Models/ParkedVehicle.cs
@@ -11,7 +11,7 @@ namespace Gitgruppen.Models
         public Type Type { get; set; }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public DateTime Arrived { get; set; }
+        public DateTime Arrived { get; set; } = DateTime.Now;
 
         public string Color { get; set; }
         public string Brand { get; set; }
