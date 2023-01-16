fc720f7648f909ec09a966b8190e65d44651d9f4
satisfly <vilkenbraadress@gmail.com>
2023-01-16 16:45:43 +0100

    Updated from develop

--- Gitgruppen/GitGruppen.Core/Member.cs
+++ Gitgruppen/GitGruppen.Core/Member.cs
@@ -13,6 +13,22 @@ namespace GitGruppen.Core
        /* public Membership Membership { get; set; }*/
 
         public ICollection<Vehicle> Vehicles { get; set; }
+
+      /*  public ICollection<Receipt> Receipts { get; set; }*/
+
+        public Boolean isValid(string pnr)
+        {
+            Regex regex = new Regex(@"^(\d{10}|\d{12}|\d{6}-\d{4}|\d{8}-\d{4}|\d{8} \d{4}|\d{6} \d{4})");
+
+            MatchCollection matches = regex.Matches(pnr);
+
+            if (matches.Count == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
   /*  public enum Membership
     {
