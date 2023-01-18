2475f5a257b80a2d9c1a2af86317f49bc3e6b1b9
Dennis Hagenäs <dh_lexicon@outlook.com>
2023-01-18 11:00:25 +0100

    initial config

---
+++ Gitgruppen/Gitgruppen/SerializeConfig/SerializeConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace Gitgruppen.SerializeConfig
+{
+    public class SerializeConfig
+    {
+        public class ConfigObject
+        {
+
+        }
+        public ConfigObject ReadFromFile() {
+            string fileName = "SerializeObject\\appconfig.json";
+            string jsonString = File.ReadAllText(fileName);
+            ConfigObject conf = JsonSerializer.Deserialize<ConfigObject>(jsonString)!;
+
+            return conf;
+        }
+
+        public async Task WriteToFileAsync(ConfigObject configObject)
+        {
+            string fileName = "SerializeObject\\appconfig.json";
+            using FileStream createStream = File.Create(fileName);
+            await JsonSerializer.SerializeAsync(createStream, configObject);
+            await createStream.DisposeAsync();
+        }
+    }
+}
